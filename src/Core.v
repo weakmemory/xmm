3edb8c3d8fd0f3fd6587e0f9ea7bbf2a2de19e29
Anton Podkopaev <podkoav239@gmail.com>
2023-10-18 15:51:26 +0200

    XMM steps: wip

--- src/Core.v
+++ src/Core.v
@@ -3,6 +3,10 @@ From PromisingLib Require Import Language Basic.
 From hahn Require Import Hahn.
 From hahnExt Require Import HahnExt.
 From imm Require Import Events Execution Execution_eco imm_s_hb.
+From imm Require Import imm_s_ppo.
+From imm Require Import imm_s_hb.
+From imm Require Import imm_bob.
+From imm Require Import SubExecution.
 
 Import ListNotations.
 
@@ -16,12 +20,16 @@ Inductive cont_label :=
 
 Section Race.
 Variable (G : execution).
-Variable (hb : relation actid).
 Notation "'E'" := (acts_set G).
 Notation "'lab'" := (lab G).
 Notation "'same_loc'" := (same_loc lab).
 Notation "'mod'" := (mod lab).
 Notation "'W'" := (is_w lab).
+Notation "'hb'" := (hb G).
+Notation "'ppo'" := (ppo G).
+Notation "'bob'" := (bob G).
+Notation "'rf'" := (rf G).
+Notation "'sb'" := (sb G).
 
 Definition one (s : actid -> Prop) : relation actid :=
     fun x y => s x \/ s y.
@@ -29,6 +37,9 @@ Definition one (s : actid -> Prop) : relation actid :=
 Definition race := restr_rel E (one W ∩ same_loc \ clos_sym hb).
 
 Definition race_mod (o : mode) := race ∩ one (fun e => mode_le (mod e) o).
+
+Definition ppo_alt := (sb ∩ same_loc ∪ bob)⁺.
+Definition hb_alt := (ppo_alt ∪ rf)⁺.
 End Race.
 
 Module Commit.
@@ -44,11 +55,12 @@ Module WCore.
 
 Record t := {
     G : execution;
-    commit_entries : Commit.id -> option Commit.entry;
-    non_commit_ids : Commit.id -> Prop;
     cont : cont_label ->
             option { lang : Language.t (list label) &
                             (Language.state lang) };
+
+    commit_entries : Commit.id -> option Commit.entry;
+    non_commit_ids : Commit.id -> Prop;
 }.
 
 Section WCoreDefs.
@@ -71,14 +83,15 @@ Definition committed : Commit.id -> Prop :=
 
 Record wf := {
     wf_G : Wf G;
+    cont_defined : forall e (NINIT : ~ is_init e) (IN : E e) (NRMW : ~ dom_rel rmw e),
+        is_some (cont X (CEvent e));
+    (* TODO: add property stating existence of continuation for some threads *)
+
     non_commit_ids_inf : set_size non_commit_ids = NOinfinity;
     non_commit_ids_no_entry : forall cid (NCI : non_commit_ids cid),
         commit_entries cid = None;
     no_entry_non_commit_ids : forall cid (CIN : commit_entries cid = None),
         non_commit_ids cid;
-    cont_defined : forall e (NINIT : ~ is_init e) (IN : E e) (NRMW : ~ dom_rel rmw e),
-        is_some (cont X (CEvent e));
-    (* TODO: add property stating existence of continuation for some threads *)
 }.
 
 Definition committed_actid_set :=
@@ -138,6 +151,38 @@ Definition basic_step_
            (X X' : t) : Prop :=
   ⟪ COMMITTED : committed X' ≡₁ committed X ⟫ /\
   basic_step_exec_ lang k k' st st' e e' (G X) (G X').
+
+Definition commit_step_
+           (cid : Commit.id)
+           (e  : actid)
+           (X X' : t) : Prop :=
+  << SAMEEXEC : G X' = G X >> /\
+  << SAMEcont : cont X' = cont X >> /\
+
+  << NEWCID   : non_commit_ids X cid >> /\
+  << NCIDS    : non_commit_ids X' ≡₁ non_commit_ids X \₁ eq cid >> /\
+  << ECID     : commit_entries X' = upd (commit_entries X) cid (Some (Commit.InExec e)) >>
+  .
+
+Definition reexec_step_
+           (X''  : t)
+
+           (w    : actid)
+           (r    : actid)
+           (X X' : t) : Prop :=
+  let E_C := committed_actid_set X in
+  << NOTCOM  : ~ E_C r >> /\
+  << HBCOM   : dom_rel (hb_alt (G X) ;; <|eq r|>) ⊆₁ E_C >> /\
+  << RR      : is_r (lab (G X)) r >> /\
+  << WW      : is_w (lab (G X)) w >> /\
+  << SAMELOC : same_loc (lab (G X)) w r >> /\
+  << RACE : (race (G X) ∪ hb (G X)) w r >> /\
+
+  (* TODO: specify sc relation *)
+  << SUBEXEC : sub_execution (G X) (G X'') ∅₂ ∅₂ >> /\
+  << EX''    : acts_set (G X'') ≡₁ acts_set (G X) \₁ codom_rel (<|eq r|>;; (sb (G X) ∪ rf (G X))⁺) >> /\
+
+  False.
 
 End WCoreSteps.
 
