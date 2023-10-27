5401a21c1225a50aa85cefbe35e05d0c5f05d572
Anton Podkopaev <podkoav239@gmail.com>
2023-10-27 13:19:37 +0200

    Removed Coq 8.17 from CI

--- .nix/config.nix
+++ .nix/config.nix
@@ -31,15 +31,6 @@
 
   default-bundle = "8.18";
 
-  bundles."8.17" = {
-    push-branches = [ "master" "main" ];
-    coqPackages.coq.override.version = "8.17";
-    coqPackages.hahn.override.version = "master";
-    coqPackages.hahnExt.override.version = "0.9.4";
-    coqPackages.sflib.override.version = "master";
-    coqPackages.promising-lib.override.version = "master";
-    coqPackages.imm.override.version = "1.6.1";
-  };
   bundles."8.18" = {
     push-branches = [ "**" ];
     coqPackages.coq.override.version = "8.18";
