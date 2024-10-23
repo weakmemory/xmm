58323097e7feb31a62c100c458f65cbbf39fffc2
InnocentusLime <innocentuslime.help@gmail.com>
2024-10-23 19:03:32 +0000

    Re-exec lemmas draft

--- src/reordering/ReorderingReexec.v
+++ src/reordering/ReorderingReexec.v
@@ -11,14 +11,20 @@ Require Import SubToFullExec.
 
 From hahn Require Import Hahn.
 From hahnExt Require Import HahnExt.
-From imm Require Import Events Execution.
+From imm Require Import Events Execution Execution_eco.
 Require Import Setoid Morphisms Program.Basics.
+Require Import xmm_s_hb.
 
-Section Reexec.
+Module ReorderingReexec.
+
+Section ReorderingReexec.
 
 Variable X_t X_t' X_s : WCore.t.
 Variable a_t b_t a_t' b_t' : actid.
 Variable mapper : actid -> actid.
+Variable f : actid -> actid.
+Variable dtrmt cmt : actid -> Prop.
+Variable l_a' : label. (* The 'desired' label for phantom a_s *)
 
 Notation "'G_t'" := (WCore.G X_t).
 Notation "'G_t''" := (WCore.G X_t').
@@ -95,171 +101,154 @@ Notation "'Rel_s'" := (Rel G_s).
 
 Notation "'Tid_' t" := (fun e => tid e = t) (at level 1).
 
-Hypothesis INV : reord_step_pred X_t a_t b_t.
-Hypothesis INV' : reord_step_pred X_t' a_t' b_t'.
+(* Conditions for the re-execution lemma *)
+Record reexec_conds : Prop := {
+  rc_a_dtrmt : dtrmt a_t <-> dtrmt a_t';
+  rc_b_dtrmt : dtrmt b_t <-> dtrmt b_t';
+  rc_a_eq : dtrmt a_t -> a_t = a_t';
+  rc_b_eq : dtrmt b_t -> b_t = b_t';
+  rc_pres : b_t = b_t' -> a_t = a_t';
+  rc_simrel : reord_simrel X_s X_t a_t b_t mapper;
+  rc_step : WCore.reexec X_t X_t' f dtrmt cmt;
+  rc_inv_start : reord_step_pred X_t a_t b_t;
+  rc_inv_end : reord_step_pred X_t' a_t' b_t';
+  rc_l_a_nacq : mode_le (WCore.lab_mode l_a') Orlx;
+}.
+
+Hypothesis CTX : reexec_conds.
+
+(* Id aliases for better readability *)
+Definition a_s := b_t.
+Definition b_s := a_t.
+Definition a_s' := b_t'.
+Definition b_s' := a_t'.
 
 Definition extra_b :=
   ifP ~E_t' a_t' /\ E_t' b_t' then ∅
   else eq b_t' ∩₁ E_t'.
 
-Lemma simrel_reexec f dtrmt cmt
-    (ADTRMT : dtrmt a_t <-> dtrmt a_t')
-    (BDTRMT : dtrmt b_t <-> dtrmt b_t')
-    (ACMT : dtrmt a_t -> a_t = a_t')
-    (BCMT : dtrmt b_t -> b_t = b_t')
-    (PRESERVATION : b_t = b_t' -> a_t = a_t')
-    (SIMREL : reord_simrel X_s X_t a_t b_t mapper)
-    (STEP : WCore.reexec X_t X_t' f dtrmt cmt) :
-  exists mapper' X_s' f' dtrmt' cmt',
-    << SIMREL : reord_simrel X_s' X_t' a_t' b_t' mapper' >> /\
-    << STEP : WCore.reexec X_s X_s' f' dtrmt' cmt' >>.
-Proof using INV INV'.
-  destruct STEP as (thrdle & STEP).
-  set (mapper' :=  upd (upd id a_t' b_t') b_t' a_t').
-  set (cmt' := mapper' ↑₁ (cmt ∪₁ extra_b)).
-  set (dtrmt' := mapper' ↑₁ (dtrmt \₁ extra_b)).
-  set (f' := mapper' ∘ f ∘ mapper').
-  set (G_s' := {|
-    acts_set := mapper' ↑₁ E_t' ∪₁ extra_a X_t' a_t' b_t' b_t';
-    threads_set := threads_set G_t';
-    lab := lab_t' ∘ mapper';
-    rf := mapper' ↑ rf_t' ∪
-          srf_s ⨾ ⦗extra_a X_t' a_t' b_t' b_t' ∩₁ R_s⦘;
-    co := mapper' ↑ co_t' ∪
-          add_max
-            (extra_co_D (mapper' ↑₁ E_t') lab_s (loc_s b_t))
-            (extra_a X_t' a_t' b_t' b_t' ∩₁ W_s);
-    rmw := mapper' ↑ rmw_t';
-    rmw_dep := rmw_dep_s;
-    ctrl := ctrl_s;
-    data := data_s;
-    addr := addr_s;
-  |}).
-  set (X_s' := {|
-    WCore.sc := WCore.sc X_s;
-    WCore.G := G_s';
-  |}).
-  assert (MAPEQ : eq_dom dtrmt mapper mapper').
-  { assert (DTRMT : dtrmt ⊆₁ E_t).
-    { rewrite (WCore.dtrmt_cmt STEP), WCore.reexec_embd_acts.
-      all: eauto using WCore.reexec_embd_corr. }
-    unfold mapper'.
-    destruct classic with (dtrmt a_t) as [DA|NDA],
-             classic with (dtrmt b_t) as [DB|NDB].
-    all: unfolder; intros x XIN.
-    all: try rewrite <- ACMT by auto.
-    all: try rewrite <- BCMT by auto.
-    all: rewrite (rsr_mapper SIMREL)
-         by (apply DTRMT in XIN; exact XIN).
-    { reflexivity. }
-    { exfalso. apply NDB. apply (WCore.reexec_dtrmt_sb_closed STEP).
-      unfolder. exists a_t, a_t; splits; auto.
-      apply DTRMT in DA.
-      unfold sb. unfolder. splits; auto; try now apply INV.
-      now apply (rsr_at_bt_ord INV). }
-    { destruct classic with (x = b_t) as [EQB|NQB].
-      { subst x. rewrite !upds. auto. }
-      rewrite !updo with (a := b_t) by exact NQB.
-      rewrite !updo; try congruence.
-      intro FALSO. subst x. now apply NDA, ADTRMT. }
-    rewrite !updo; try congruence.
-    { intro FALSO. subst x. now apply NDA, ADTRMT. }
-    intro FALSO. subst x. now apply NDB, BDTRMT. }
-  assert (MAPINJ : inj_dom E_t' mapper').
-  { unfold inj_dom, mapper'. intros x y XIN YIN FEQ.
-      destruct classic with (x = a_t') as [XEQA|XNQA],
-               classic with (x = b_t') as [XEQB|XNQB],
-               classic with (y = a_t') as [YEQA|YNQA],
-               classic with (y = b_t') as [YEQB|YNQB].
-      all: try subst x; try subst y.
-      all: try congruence.
-      all: try now (exfalso; apply (rsr_at_neq_bt INV'); congruence).
-      all: rewrite ?upds in FEQ.
-      all: rewrite 1?updo in FEQ; try congruence.
-      all: rewrite ?upds in FEQ; try congruence.
-      all: unfold id in FEQ.
-      all: rewrite 2?updo in FEQ; try congruence.
-      all: rewrite ?upds in FEQ; try congruence.
-      rewrite updo in FEQ; try congruence. }
-  assert (SIMREL' : reord_simrel X_s' X_t' a_t' b_t' mapper').
-  { constructor; ins.
-    { unfold extra_a; desf. intros x XEQ. subst x.
-      constructor; ins.
-      admit. }
-    { rewrite set_minus_union_l, set_minusK, set_union_empty_r.
-      rewrite set_minus_disjoint; [reflexivity |].
-      apply set_disjointE. split; [| basic_solver].
-      unfold extra_a; desf; [| clear; basic_solver].
-      unfold mapper', id. unfolder.
-      intros x ((y & YINE & MAP) & XEQ). subst x.
-      destruct classic with (y = b_t') as [YEQB | YNQB].
-      { subst y. rewrite upds in XEQ. apply (rsr_at_neq_bt INV').
-        congruence. }
-      rewrite updo in XEQ; [| congruence].
-      destruct classic with (y = a_t') as [YEQA | YNQA].
-      { subst y. desf. }
-      rewrite updo in XEQ; congruence. }
-    { unfolder. unfold mapper', id. intros x INIT.
-      rewrite !updo; auto.
-      all: intro FALSO; subst x.
-      { now apply (rsr_at_ninit INV'). }
-      now apply (rsr_bt_ninit INV'). }
-    { unfold mapper', id, compose. unfolder.
-      intros x XIN.
-      destruct classic with (x = a_t') as [XEQA|XNQA],
-               classic with (x = b_t') as [XEQB|XNQB].
-      all: try subst x.
-      all: rupd; try now apply INV'.
-      all: symmetry; apply INV'. }
-    { unfold mapper', id, compose. unfolder.
-      intros x _.
-      destruct classic with (x = a_t') as [XEQA|XNQA],
-               classic with (x = b_t') as [XEQB|XNQB].
-      all: try subst x.
-      all: try now (exfalso; try now apply (rsr_at_neq_bt INV')).
-      { rewrite updo with (c := a_t'); [| apply INV'].
-        now rewrite !upds. }
-      { rewrite upds, updo with (c := a_t'); [| apply INV'].
-        now rewrite upds. }
-      rewrite !updo with (c := x); auto. }
-    { admit. }
-    { admit. }
-    { admit. }
-    { admit. }
-    { admit. }
-    { admit. }
-    { admit. }
-    { admit. }
-    { unfold mapper', id. clear. unfolder.
-      ins. desf. rewrite !updo; congruence. }
-    { unfold mapper', id. clear. unfolder.
-      ins. desf. now rewrite upds. }
-    unfold mapper', id. clear - INV'. unfolder.
-    ins. desf. rewrite updo, upds; auto.
-    apply INV'. }
-  exists mapper', X_s', f', dtrmt', cmt'.
-  unfold NW; split; auto.
-  red.
-  exists thrdle.
-  constructor; ins; unfold dtrmt', cmt'.
-  { admit. }
-  { rewrite (WCore.reexec_embd_dom STEP).
-    unfold extra_b, extra_a; desf.
-    all: rewrite ?set_union_empty_r.
-    { auto with hahn. }
-    destruct classic
-        with (E_t' b_t')
-          as [INB|NINB].
-    { rewrite set_union_absorb_r; auto with hahn.
-      clear. basic_solver. }
-    arewrite (eq b_t' ∩₁ E_t' ≡₁ ∅).
-    { clear - NINB. basic_solver. }
-    clear. basic_solver. }
-  { admit. }
+Definition mapper' := upd (upd id a_t' a_s') b_t' b_s'.
+
+Lemma mapeq : eq_dom dtrmt mapper mapper'.
+Proof using CTX.
   admit.
 Admitted.
 
-End Reexec.
+Lemma mapinj : inj_dom E_t' mapper'.
+Proof using CTX.
+  admit.
+Admitted.
+
+Definition cmt' := mapper' ↑₁ (cmt ∪₁ extra_b).
+Definition dtrmt' := mapper' ↑₁ (dtrmt \₁ extra_b).
+Definition f' := mapper' ∘ f ∘ mapper'.
+(* Intermediate graph for locating the srf edge for for a_s *)
+Definition G_s'' : execution := {|
+  acts_set := mapper' ↑₁ E_t';
+  threads_set := threads_set G_t';
+  lab := lab_t' ∘ mapper';
+  rf := mapper' ↑ rf_t';
+  co := mapper' ↑ co_t';
+  rmw := mapper' ↑ rmw_t';
+  rmw_dep := rmw_dep_s;
+  ctrl := ctrl_s;
+  data := data_s;
+  addr := addr_s;
+|}.
+
+Lemma intermediate_graph_wf
+    (INB : E_t' b_t')
+    (NINA : ~ E_t' a_t') :
+  Wf G_s''.
+Proof using CTX.
+  admit. (* This graph is very conveniently mapped by m *)
+Admitted.
+
+Lemma intermediate_graph_cons
+    (INB : E_t' b_t')
+    (NINA : ~ E_t' a_t') :
+  irreflexive (hb G_s'' ⨾ (eco G_s'')^?).
+Proof using CTX.
+  admit. (* True by monotonicity *)
+Admitted.
+
+Definition A_s' := extra_a X_t' a_t' b_t' a_s'.
+Definition E_s' := mapper' ↑₁ E_t' ∪₁ A_s'.
+Definition sb_s' := ⦗E_s'⦘ ⨾ ext_sb ⨾ ⦗E_s'⦘.
+
+(* 'fake' srf. we later show that it is equal to srf ; [a_s'] *)
+Definition srf_s'' :=
+  (
+    vf_s ⨾ sb_s' ⨾ ⦗A_s'⦘ ∩
+    (fun x _ => loc_s x = WCore.lab_loc l_a')
+  ) ⨾ ⦗WCore.lab_is_r l_a'⦘
+    \ (co_s ⨾ vf_s ⨾ sb_s' ⨾ ⦗A_s'⦘).
+
+(* Some fake srf props *)
+
+(* The 'final' graph *)
+Definition G_s' : execution := {|
+  acts_set := mapper' ↑₁ E_t' ∪₁ A_s';
+  threads_set := threads_set G_t';
+  lab := lab_t' ∘ mapper';
+    rf := mapper' ↑ rf_t' ∪
+          srf_s'';
+    co := mapper' ↑ co_t' ∪
+          add_max
+            (extra_co_D (mapper' ↑₁ E_t') lab_s (WCore.lab_loc l_a'))
+            (A_s' ∩₁ WCore.lab_is_w l_a');
+  rmw := mapper' ↑ rmw_t';
+  rmw_dep := rmw_dep_s;
+  ctrl := ctrl_s;
+  data := data_s;
+  addr := addr_s;
+|}.
+Definition X_s' := {|
+  WCore.sc := WCore.sc X_s;
+  WCore.G := G_s';
+|}.
+
+Lemma final_graph_with_srf_wf
+    (INB : E_t' b_t')
+    (NINA : ~ E_t' a_t') :
+  Wf G_s''.
+Proof using CTX.
+  admit. (* We added an edge and a_s to intermediate graph *)
+Admitted.
+
+Lemma final_graph_with_srf_cons
+    (INB : E_t' b_t')
+    (NINA : ~ E_t' a_t') :
+  irreflexive (hb G_s'' ⨾ (eco G_s'')^?).
+Proof using CTX.
+  admit. (* TODO (ask Evgenii) *)
+Admitted.
+
+(* Srf goodness *)
+Lemma final_graph_with_srf_srf :
+  (rf G_s'') ⨾ ⦗A_s'⦘ ≡ (srf G_s'') ⨾ ⦗A_s'⦘.
+Proof using CTX.
+  admit.
+Admitted.
+
+Lemma final_graph_simrel :
+  reord_simrel X_s' X_t' a_t' b_t' mapper'.
+Proof using CTX.
+  constructor; ins.
+  { apply mapinj. }
+  { admit. }
+Admitted.
+
+Lemma simrel_reexec :
+  exists mapper' X_s' f' dtrmt' cmt',
+    << SIMREL : reord_simrel X_s' X_t' a_t' b_t' mapper' >> /\
+    << STEP : WCore.reexec X_s X_s' f' dtrmt' cmt' >>.
+Proof using CTX.
+Admitted.
+
+End ReorderingReexec.
+
+End ReorderingReexec.
 
 (* DRAFTS FOR FINAL THEOREM *)
 
