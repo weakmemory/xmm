af64de17cac6a43facc1aaeabf234d0aa2e2952f
keba4ok <77245196+keba4ok@users.noreply.github.com>
2025-02-26 06:19:39 +0100

    drafting

--- src/sequentialization/Sequent.v
+++ src/sequentialization/Sequent.v
@@ -8,6 +8,7 @@ Require Import AuxInj.
 Require Import xmm_s_hb.
 Require Import Lia.
 From xmm Require Import Reordering.
+From xmm Require Import ThreadTrace.
 
 From hahn Require Import Hahn.
 From hahnExt Require Import HahnExt.
@@ -65,8 +66,10 @@ Definition po_seq := (Tid_ t_1 ∩₁ E_s) × (Tid_ t_2 ∩₁ E_s).
 
 Record seq_simrel : Prop := {
     seq_inj : inj_dom E_t mapper;
-    seq_tid_1 : forall tid, tid <> Tid_ t_2 -> eq_dom E_t (tid ∘ mapper) tid; 
-    seq_tid_2 : eq_dom E_t (Tid_ t_2 ∘ mapper) Tid_ t_1;
+
+    seq_tid_1 : forall e : actid, E_t e -> tid (mapper e) <> t_2 -> tid e = tid (mapper e);
+    seq_tid_2 : forall e : actid, E_t e -> tid (mapper e) = t_2 -> tid e = t_1;
+
     seq_lab : eq_dom E_t (lab_s ∘ mapper) lab_t;
     seq_acts : E_s ≡₁ mapper ↑₁ E_t;
     seq_sb : sb_s ∪ po_seq ≡ mapper ↑ sb_t;
@@ -100,24 +103,24 @@ Lemma seq_simrel_init threads
     (NINIT1 : t_1 <> tid_init)
     (NINIT2 : t_2 <> tid_init)
     (THRD1 : threads t_1)
-    (THRD2 : threads t_2)
     (THRDNEQ : t_1 <> t_2)
     (INIT : threads tid_init) :
   << SIMREL : seq_simrel
-    (WCore.Build_t (WCore.init_exec threads) ∅₂)
+    (WCore.Build_t (WCore.init_exec (threads ∪₁ eq t_2)) ∅₂)
     (WCore.Build_t (WCore.init_exec threads) ∅₂)
     t_1 t_2
     id >>.
 Proof using.
     assert (IWF : Wf (WCore.init_exec threads)).
     { now apply WCore.wf_init_exec. }
-    split; red; vauto; ins.
-    { unfold compose. unfold is_init in DX. desf.
-      unfold tid. desf. unfold not in *.
-      apply PropExtensionality.propositional_extensionality.
-      split; ins; desf. }
+    split; vauto; ins.
+    { assert (FALSE : t_2 = tid_init).
+      { rewrite <- H0. unfold tid. desf.
+        unfold is_init in H. desf. }
+      desf. }
     { clear; basic_solver. }
-    { unfold po_seq; ins.
+    { rewrite collect_rel_id; split; vauto.
+      unfold po_seq; ins.
       assert (EMP1 : (fun e : actid => tid e = t_1)
                     ∩₁ (fun a : actid => is_init a) ≡₁ ∅).
       { split; [|basic_solver].
@@ -129,11 +132,7 @@ Proof using.
         intros x COND. destruct COND as [TID ISINIT].
         unfold is_init in ISINIT. desf. }
       rewrite EMP1, EMP2. clear; basic_solver 8. }
-    { clear; basic_solver. }
-    { clear; basic_solver. }
-    { clear; basic_solver. }
-    { basic_solver. }
-    destruct H; basic_solver.
+    all : clear; basic_solver.
 Qed.
 
 End SeqSimrelInit.
@@ -217,18 +216,6 @@ Notation "'R_s'" := (fun x => is_true (is_r lab_s x)).
 Notation "'F_s'" := (F G_s).
 
 Notation "'Tid_' t" := (fun e => tid e = t) (at level 1).
-
-(* Definition mapper' := upd mapper e e. *)
-
-(* Lemma seq_actsE
-    (NINIT1 : t_1 <> tid_init)
-    (NINIT2 : t_2 <> tid_init)
-    (THRDNEQ : t_1 <> t_2)
-    (SIMREL : seq_simrel X_s X_t t_1 t_2 mapper) :
-  E_s ≡₁ E_t.
-Proof using.
-  admit.
-Admitted. *)
 
 Lemma simrel_step_e
     (n : nat)
@@ -363,28 +350,21 @@ Proof using.
           { clear. basic_solver. }
           rewrite MAPER_E, MAPSUB, (seq_codom SIMREL).
           clear - NOTIN. basic_solver. }
-        { intros tid TIDNEQ. rewrite (WCore.add_event_acts ADD).
-          apply eq_dom_union; split.
-          { destruct SIMREL. intros x COND.
-            unfold compose. unfold mapper'. rewrite updo.
-            { apply seq_tid_3; auto. }
-            intros EQ. basic_solver. }
-          unfold mapper'. intros x COND.
-          unfold compose. subst x.
-          rewrite upds; vauto. }
-        { rewrite (WCore.add_event_acts ADD).
-          rewrite eq_dom_union; splits.
-          { destruct SIMREL. unfold mapper'.
-            intros x COND. unfold compose.
-            rewrite updo.
-            { apply seq_tid_4; auto. }
-            intros EQ. basic_solver. }
-          intros x COND. unfold compose.
-          unfold mapper'; subst. rewrite upds.
-          admit. (*TODO : messed up*) }
+        { intros ev INE' TIDCOND.
+          destruct classic with (ev = e) as [EQ | NEQ].
+          { subst ev. unfold mapper'. rewrite upds; vauto. }
+          unfold mapper'. rewrite updo; vauto.
+          apply (seq_tid_1 SIMREL); vauto.
+          apply EQACTS in INE'. destruct INE' as [C1 | C2]; vauto.
+          unfold mapper' in TIDCOND. rewrite updo in TIDCOND; vauto. }
+        { intros ev INE' TIDCOND.
+          destruct classic with (ev = e) as [EQ | NEQ]; vauto.
+          apply (seq_tid_2 SIMREL); vauto.
+          { apply EQACTS in INE'. destruct INE' as [C1 | C2]; vauto. }
+          unfold mapper'. rewrite updo; vauto. }
         { intros x COND. unfold compose.
           arewrite (mapper' (mapper' x) = x); vauto. }
-        { admit. }
+        { admit. (*TODO : po-work*) }
         { rewrite (seq_threads SIMREL).
           destruct ADD. rewrite add_event_threads; vauto. }
         unfold mapper'. intros x COND.
@@ -459,17 +439,25 @@ Proof using.
           { clear. basic_solver. }
           rewrite MAPER_E, MAPSUB, (seq_codom SIMREL).
           admit. }
-        { intros tid TIDNEQ. rewrite (WCore.add_event_acts ADD).
-          apply eq_dom_union; split.
-          { destruct SIMREL. intros x COND.
-            unfold compose. unfold mapper'. rewrite updo.
-            { apply seq_tid_3; auto. }
-            intros EQ. basic_solver. }
-          unfold mapper'. intros x COND.
-          unfold compose. subst x.
-          rewrite upds; vauto.
-          admit. }
-        { rewrite (WCore.add_event_acts ADD).
+        { intros ev INE' TIDCOND.
+          destruct classic with (ev = e) as [EQ | NEQ].
+          { subst ev. unfold mapper'. rewrite upds; vauto.
+            unfold mapper' in TIDCOND. rewrite upds in TIDCOND; vauto. }
+          unfold mapper'. rewrite updo; vauto.
+          apply (seq_tid_1 SIMREL); vauto.
+          apply EQACTS in INE'. destruct INE' as [C1 | C2]; vauto.
+          unfold mapper' in TIDCOND. rewrite updo in TIDCOND; vauto. }
+        { intros ev INE' TIDCOND.
+          destruct classic with (ev = e) as [EQ | NEQ]; vauto.
+          { rewrite EQ; vauto. }
+          apply (seq_tid_2 SIMREL); vauto.
+          { apply EQACTS in INE'. destruct INE' as [C1 | C2]; vauto.
+            exfalso; basic_solver. }
+          unfold mapper' in TIDCOND.
+          rewrite updo in TIDCOND; vauto. }
+        { unfold compose.
+        
+        rewrite (WCore.add_event_acts ADD).
           rewrite eq_dom_union; splits.
           { destruct SIMREL. unfold mapper'.
             intros x COND. unfold compose.
@@ -717,10 +705,96 @@ Admitted.
 
 End SimrelGen.
 
+Section BehaviorGraph.
+
+Variable G_1 G_2 : execution.
+
+Notation "'E_1'" := (acts_set G_1).
+
+Notation "'lab'" := (lab G_1).
+Notation "'loc'" := (loc lab).
+Notation "'val'" := (val lab).
+
+Definition graph_locations (G : execution) : Set :=
+  { l : location | exists e, acts_set G e /\ loc e = Some l }.
+
+Definition same_behaviors (G_1 G_2 : execution) : Prop :=
+  behavior_spec G_1 = behavior_spec G_2.
+
+End BehaviorGraph.
+
 Section SimrelMain.
 
 Variable X_t_init X_s_init X_t : WCore.t.
 Variable t_1 t_2 : thread_id.
+
+Notation "'G_t_init'" := (WCore.G X_t_init).
+Notation "'G_s_init'" := (WCore.G X_s_init).
+Notation "'G_t'" := (WCore.G X_t).
+
+Notation "'R' G" := (fun e => is_true (is_r (lab G) e)) (at level 1).
+Notation "'F' G" := (fun e => is_true (is_f (lab G) e)) (at level 1).
+Notation "'W' G" := (fun e => is_true (is_w (lab G) e)) (at level 1).
+Notation "'Acq' G" := (fun e => is_true (is_acq (lab G) e)) (at level 1).
+Notation "'Rlx' G" := (fun e => is_true (is_rlx (lab G) e)) (at level 1).
+Notation "'Rel' G" := (fun e => is_true (is_rel (lab G) e)) (at level 1).
+
+Notation "'lab_t_init'" := (lab G_t_init).
+Notation "'val_t_init'" := (val lab_t_init).
+Notation "'loc_t_init'" := (loc lab_t_init).
+Notation "'same_loc_t_init'" := (same_loc lab_t_init).
+Notation "'E_t_init'" := (acts_set G_t_init).
+Notation "'sb_t_init'" := (sb G_t_init).
+Notation "'rf_t_init'" := (rf G_t_init).
+Notation "'co_t_init'" := (co G_t_init).
+Notation "'rmw_t_init'" := (rmw G_t_init).
+Notation "'rpo_t_init'" := (rpo G_t_init).
+Notation "'rmw_dep_t_init'" := (rmw_dep G_t_init).
+Notation "'data_t_init'" := (data G_t_init).
+Notation "'ctrl_t_init'" := (ctrl G_t_init).
+Notation "'addr_t_init'" := (addr G_t_init).
+Notation "'W_t_init'" := (fun x => is_true (is_w lab_t_init x)).
+Notation "'R_t_init'" := (fun x => is_true (is_r lab_t_init x)).
+Notation "'Loc_t_init_' l" := (fun e => loc_t_init e = l) (at level 1).
+
+Notation "'lab_s_init'" := (lab G_s_init).
+Notation "'val_s_init'" := (val lab_s_init).
+Notation "'loc_s_init'" := (loc lab_s_init).
+Notation "'same_loc_s_init'" := (same_loc lab_s_init).
+Notation "'E_s_init'" := (acts_set G_s_init).
+Notation "'loc_s_init'" := (loc lab_s_init).
+Notation "'sb_s_init'" := (sb G_s_init).
+Notation "'rf_s_init'" := (rf G_s_init).
+Notation "'co_s_init'" := (co G_s_init).
+Notation "'rmw_s_init'" := (rmw G_s_init).
+Notation "'rpo_s_init'" := (rpo G_s_init).
+Notation "'rmw_dep_s_init'" := (rmw_dep G_s_init).
+Notation "'data_s_init'" := (data G_s_init).
+Notation "'ctrl_s_init'" := (ctrl G_s_init).
+Notation "'addr_s_init'" := (addr G_s_init).
+Notation "'W_s_init'" := (fun x => is_true (is_w lab_s_init x)).
+Notation "'R_s_init'" := (fun x => is_true (is_r lab_s_init x)).
+Notation "'Loc_s_init_' l" := (fun e => loc_s_init e = l) (at level 1).
+
+Notation "'lab_t'" := (lab G_t).
+Notation "'val_t'" := (val lab_t).
+Notation "'loc_t'" := (loc lab_t).
+Notation "'same_loc_t'" := (same_loc lab_t).
+Notation "'E_t'" := (acts_set G_t).
+Notation "'sb_t'" := (sb G_t).
+Notation "'rf_t'" := (rf G_t).
+Notation "'co_t'" := (co G_t).
+Notation "'rmw_t'" := (rmw G_t).
+Notation "'rpo_t'" := (rpo G_t).
+Notation "'rmw_dep_t'" := (rmw_dep G_t).
+Notation "'data_t'" := (data G_t).
+Notation "'ctrl_t'" := (ctrl G_t).
+Notation "'addr_t'" := (addr G_t).
+Notation "'W_t'" := (fun x => is_true (is_w lab_t x)).
+Notation "'R_t'" := (fun x => is_true (is_r lab_t x)).
+Notation "'Loc_t_' l" := (fun e => loc_t e = l) (at level 1).
+
+Notation "'Tid_' t" := (fun e => tid e = t) (at level 1).
 
 Lemma simrel_main
     (NINIT1 : t_1 <> tid_init)
@@ -730,6 +804,9 @@ Lemma simrel_main
   exists X_s mapper,
     << SIMREL : seq_simrel X_s X_t t_1 t_2 mapper >> /\
     << STEP : xmm_step＊ X_s_init X_s >> /\
-    << BEHR : 
+    << BEHRS : same_behaviors (WCore.G X_s) G_t >>.
+Proof using.
+  admit.
+Admitted.
 
 End SimrelMain.
